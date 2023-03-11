6ea14a250cf3ba2aa7c2ecd77b0c596ba7523fe0
Ubuntu <ubuntu@ip-172-31-40-237.eu-north-1.compute.internal>
2023-03-11 11:29:42 +0000

    Update 1

--- script.sh
+++ script.sh
@@ -3,13 +3,11 @@
 # Stop apache service
 sudo systemctl stop apache2
 
-cd /home/ubuntu/SiteApache
-
 # Delete old site files
 sudo rm -f /var/www/html/*
 
-# Copy new version of the site
-sudo cp -r SiteApache/site/* /var/www/html/
+# Update index.html with new version
+sudo cp site/index.html /var/www/html/
 
 # Start apache service
 sudo systemctl start apache2
